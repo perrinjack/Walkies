32d560bc9882dfd12ea4c9e952d7134a52fc5159
J Perrin <perrinjack96@gmail.com>
2020-07-15 15:55:30 +0100

    GREEN -returns false for route of over 10 units

--- lib/walkies.rb
+++ lib/walkies.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-# def ten_minute_walk?(route)
-#   return false if route.length > 10
-# end
+def ten_minute_walk?(route)
+  return false if route.length > 10
+end
--- spec/walkies_spec.rb
+++ spec/walkies_spec.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
+require './lib/walkies.rb'
 
 describe 'ten_minute_walk?' do
-    it 'returns false when the length of walk is over 10 units' do
-        expect(ten_minute_walk?(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w','E'])).to eq false
-    end
+  it 'returns false when the length of walk is over 10 units' do
+    expect(ten_minute_walk?(%w[w s e e n n e s w w E])).to eq false
+  end
 end
