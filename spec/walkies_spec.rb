ee80b4aa83f4f00ba60121a9d132f1e62e03656f
J Perrin <perrinjack96@gmail.com>
2020-07-15 15:58:10 +0100

    RED returns true for x direction totalling 0

--- spec/walkies_spec.rb
+++ spec/walkies_spec.rb
@@ -6,4 +6,8 @@ describe 'ten_minute_walk?' do
   it 'returns false when the length of walk is over 10 units' do
     expect(ten_minute_walk?(%w[w s e e n n e s w w E])).to eq false
   end
+
+  it 'returns true when x direction coordinates sum to 0' do
+    expect(ten_minute_walk?(['e','w'])).to eq true
+  end
 end
