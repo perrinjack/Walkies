f2ede4c02c365fd3dd29b062eba5a8ced0a201d5
J Perrin <perrinjack96@gmail.com>
2020-07-15 16:55:46 +0100

    rubocop run

--- spec/walkies_spec.rb
+++ spec/walkies_spec.rb
@@ -28,6 +28,6 @@ describe 'ten_minute_walk?' do
   end
 
   it 'returns false when walk DOES NOT return to spot in 10 minutes' do
-  expect(ten_minute_walk?(['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w'])).to eq false
+    expect(ten_minute_walk?(%w[w s e n n e s w w w])).to eq false
   end
 end
